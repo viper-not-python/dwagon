3e98f89f7a113202f6f1b5b3cf7fbffd2345d80c
viper-not-python <amg2004@outlook.com>
2022-05-12 14:31:38 +0200

    some fixes
    nothing world changing

--- dwagon/MyWorld.java
+++ dwagon/MyWorld.java
@@ -16,12 +16,9 @@ public class MyWorld extends World
     public MyWorld()
     {    
         // Create a new world with 1080 x 720 cells with a cell size of 1x1 pixels.
-        super(1080 , 720, 1); 
+        super(1080 , 720, 1);
         
         addObject(new DWAGON(), 300, 360);
-        
-        //PIPE pipe = new PIPE();
-        //addObject(pipe, 0, 0);
         
         addObject(new PIPE(), 600, 300);
         addObject(new PIPE(), 900, 300); 
